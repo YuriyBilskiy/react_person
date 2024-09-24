afb1ec8cc191b22f7a7774d84756b9721f11fc52
Yuriy Bilskiy <yuriy.bilskiy@gmail.com>
2024-09-24 13:00:55 +0300

    made ternaries

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
+  const selectedMarried = `${partnerName} is my ${sex === 'f' ? 'husband' : 'wife'}`;
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
       {isMarried ? (
-        <p className="Person__partner">
-          {partnerName} is my {sex === 'f' ? 'husband' : 'wife'}
-        </p>
+        <p className="Person__partner">{selectedMarried}</p>
       ) : (
         <p className="Person__partner">is not married</p>
       )}
